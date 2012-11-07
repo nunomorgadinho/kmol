e4001563d7da56060141f5f7846e775076f8e573
Dylan Martins Morais <dylan.mm@gmail.com>
2012-11-07 17:28:41 +0000

    Página de perfil.

--- wp-content/themes/kmol/content-profile.php
+++ wp-content/themes/kmol/content-profile.php
@@ -14,7 +14,86 @@
 	</header><!-- .entry-header -->
 <span class="clear"></span>
 	<div class="grid_8 alpha omega">
-		Wazza!!!
+		
+		<div class="profile_title">
+			Ana Neves
+		</div>
+		<div class="profile_description">
+			É sócia-gerente da knowman - Consultadoria em Gestão, Lda, empresa através da qual presta apoio de consultadoria nas áreas de 
+			gestão de conhecimento, aprendizagem organizacional, mudança cultural e social media. Tem participado como oradora convidada 
+			em conferências e facilitado workshops em Portugal, Brasil e Inglaterra. Criou e mantem o KMOL. Perfil no LinkedIn | Twitter
+		</div>
+		<div class="profile_texts">
+			Textos de Ana Neves no KMOL
+		</div>
+		<div class="profile_texts_info">
+			<div class="profile_text_title">
+				Influenciando comportamentos
+				<span class="news_meta">
+					12 jun - 2012
+				</span>
+			</div>
+			<div class="profile_text_short">
+				Através de um amigo meu vi no Facebook um vídeo fantástico, daqueles que nos faz sorrir e pensar que há pessoas boas. 
+				Partilho aqui o vídeo para que não fiquem com inveja e possam partilhar destes mesmos sorrisos.
+			</div>
+
+			<span class="readmore_single alignright">Ler Mais...</span>
+		</div>
+
+		<div class="profile_texts_info">
+			<div class="profile_text_title">
+				Influenciando comportamentos
+				<span class="news_meta">
+					12 jun - 2012
+				</span>
+			</div>
+			<div class="profile_text_short">
+				Através de um amigo meu vi no Facebook um vídeo fantástico, daqueles que nos faz sorrir e pensar que há pessoas boas. 
+				Partilho aqui o vídeo para que não fiquem com inveja e possam partilhar destes mesmos sorrisos.
+			</div>
+
+			<span class="readmore_single alignright">Ler Mais...</span>
+		</div>
+
+		<div class="profile_texts_info">
+			<div class="profile_text_title">
+				Influenciando comportamentos
+				<span class="news_meta">
+					12 jun - 2012
+				</span>
+			</div>
+			<div class="profile_text_short">
+				Através de um amigo meu vi no Facebook um vídeo fantástico, daqueles que nos faz sorrir e pensar que há pessoas boas. 
+				Partilho aqui o vídeo para que não fiquem com inveja e possam partilhar destes mesmos sorrisos.
+			</div>
+
+			<span class="readmore_single alignright">Ler Mais...</span>
+		</div>
+
+			<div class="more_single alignright">
+				 <?php _e ('Artigos mais antigos','kmol'); ?>
+			</div>  
+
+
+			<div class="profile_texts">
+			Comentários de Ana Neves no KMOL
+			</div>
+			<div class="profile_comments">
+				<p><a href="#">The Wisdom of Crowds: Why the Many Are Smarter Than the Few | 24 Jul 2012</a></p>
+				<p><a href="#">Ferramentas sociais de baixo custo para as organizações | 24 May 2012</a></p>
+				<p><a href="#">Reynolds Porter Chamberlain LLP | 21 May 2012</a></p>
+				<p><a href="#">Serendipidade: acaso e perspicácia | 30 Mar 2012</a></p>
+				<p><a href="#">Apontamentos do Enterprise 2.0 Summit em Paris | 5 Mar 2012</a></p>
+				<p><a href="#">Ovelhas brancas e ovelhas pretas: diferentes só na cor | 2 Mar 2012</a></p>
+				<p><a href="#">Serendipidade: acaso e perspicácia | 23 Jan 2012</a></p>
+				<p><a href="#">Eu sou as minhas redes | 19 Jan 2012</a></p>
+			</div>
+
+			<div class="more_single alignright">
+				 <?php _e ('Comentários mais antigos','kmol'); ?>
+			</div> 
+
 	</div><!-- .entry-content -->
 
 
@@ -23,7 +102,42 @@
 </article><!-- #post-<?php the_ID(); ?> -->
 <div class="alignright alpha">
 <div class="grid_3 alpha omega">
-Hello!!!
+	<div class="profile_sidebar">
+		<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/ananeves_profile.png"/>
+		<div class="sharing_post alignright sharing_profile">
+			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/mail_share.png"/>
+			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/twitter_share.png"/>
+			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/facebook_share.png"/>
+			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/linkedin_share.png"/>
+		</div>
+		<span class="clear"></span>
+
+		<div class="profile_sidebar_box">
+			<h1>sumário</h1>
+			<h2>Função: Consultora</h2>
+			<h2>Organização: Knowman</h2>
+			<h2>Sector: Serviços de consultadoria</h2>
+		</div>	
+
+		<div class="profile_sidebar_box">
+			<h1>contactos</h1>
+			<h2>Email: editor@kmol.online.pt</h2>
+			<h2>Website: www.knowman.pt</h2>
+		</div>	
+
+		<div class="profile_sidebar_box">
+			<h1>interesses</h1>
+			<h2>Gestão de conhecimento, </h2>
+			<h2>Aprendizagem organizacional, </h2>
+			<h2>Comunidades, </h2>
+			<h2>Redes sociais, </h2>
+			<h2>Web 2.0, </h2>
+			<h2>Mudança cultural </h2>
+		</div>	
+	</div>
+
+
+
 	</div>
 </div>
 
