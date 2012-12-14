62011334c1f7aa4aac5b65e81da0cfdfa52720c7
aaires <ana.aires@gmail.com>
2012-12-14 15:24:39 +0000

    page blog and single

--- wp-content/themes/kmol/content-single.php
+++ wp-content/themes/kmol/content-single.php
@@ -15,42 +15,12 @@
 			<span class="clear"></span>
 
 			<div class="tag_marcador alignleft"><?php /* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', ', ' );
-
-			if ( ! kmol_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'kmol' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'kmol' );
-				}
-
-			} else {
-				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'kmol' );
-				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'kmol' );
-				}
-
-			} // end check for categories on this blog
-
-			printf(
-				//$meta_text,
-				//$category_list,
-				$tag_list
-				//get_permalink(),
-				//the_title_attribute( 'echo=0' )
-			);
+				$tag_list = get_the_tag_list( '', ' ' );
+				printf($tag_list);
 		?></div>
 		
-		<div class="sharing_post alignright">
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/mail_share.png"/>
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/twitter_share.png"/>
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/facebook_share.png"/>
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/linkedin_share.png"/>
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/comment_share.png"/>
-		</div>
+		<?php get_template_part( 'content', 'share' ); ?>
+		
 	</header><!-- .entry-header -->
 
 	<span class="clear"></span>
@@ -65,29 +35,44 @@
 
 		<!-- <?php edit_post_link( __( 'Edit', 'kmol' ), '<span class="edit-link">', '</span>' ); ?> -->
 
-	<div class="sharing_post alignright">
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/mail_share.png"/>
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/twitter_share.png"/>
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/facebook_share.png"/>
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/linkedin_share.png"/>
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/comment_share.png"/>
-		</div>
+		<?php get_template_part( 'content', 'share' ); ?>
 
 <span class="clear"></span>
 		<div class="author_container">
-
-			<img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/ivo_granja.png"/>
-			<div class="author_title">Ivo Granja</div>
-			<div class="author_description">Licenciado em Economia, a concluir o Mestrado em Inteligência Competitiva; 
-				LinkedIn: http://pt.linkedin.com/in/ivogranja</div>
+		
+			<?php 
+			if(function_exists('get_cimyFieldValue')){
+				$value = get_cimyFieldValue(get_the_author_meta('ID'), 'FOTO');
+				echo '<img src="'.get_bloginfo('siteurl').$value.'"/>';
+			}
+			?>
+			<div class="author_title"><?php echo get_the_author_meta('first_name').' '.get_the_author_meta('last_name');?></div>
+			<div class="author_description">
+				<?php echo get_the_author_meta('description');?>
+				
+				<div id="author-link">
+					<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
+						<?php printf( __( 'Ver todos os artigos de %s <span class="meta-nav">&rarr;</span>', 'kmol' ), get_the_author_meta('first_name').' '.get_the_author_meta('last_name') ); ?>
+					</a>
+				</div><!-- #author-link	-->
+				
+			</div> <!-- .author_description -->
 				<div class="autor_tags tag_marcador">
-						<a href="/">gestão_de_conhecimento</a>
-                        <a href="/">ferramentas_sociais</a>
-                        <a href="/">tecnologia web2.0</a>
-                        <a href="/">cultura_organizacional</a>
+					<?php 
+					if(function_exists('get_cimyFieldValue')){
+					
+						$value = get_cimyFieldValue(get_the_author_meta('ID'), 'AREAS');
+						$avalue = explode(',',$value);
+						foreach ($avalue as $area)
+						{
+							echo '<span>'.$area.'</span>';
+						}
+					}
+					?>
+						
 				</div>
 
-		</div>
+		</div> <!-- author_container -->
 
 	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
