db4e47fb9f6315466ca712c0ec4768a13a4bbb62
Dylan Martins Morais <dylan.mm@gmail.com>
2012-12-17 16:48:57 +0000

    Ajustes nos marcadores_v2

--- wp-content/themes/kmol/content-markers.php
+++ wp-content/themes/kmol/content-markers.php
@@ -16,8 +16,9 @@
 		<div class="grid_4 alpha marcador">
 			<h1 class="marcador_title"><?php _e('Entrevistas','kmol'); ?></h1>
 			<div class="marcador_short">
+				<div class="marcador_subtitle_container">
 				<h2 class="marcador_subtitle"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-				<div class="news_meta"><?php echo "Por ".get_the_author_meta('nicename');?>, <?php kmol_posted_on();?></div>
+				<div class="news_meta"><?php echo "Por ".get_the_author_meta('nicename');?>, <?php kmol_posted_on();?></div></div>
 				 <?php if(has_post_thumbnail()){?> <div class="marcador_img"><?php the_post_thumbnail('thumbnail');?> </div> <?php }?>
 				<div class="marcador_description"><?php wpe_excerpt('wpe_excerptlength_small', 'new_excerpt_more');?></div>
 			</div>
@@ -39,8 +40,9 @@
 		<div class="grid_4 marcador">
 			<h1 class="marcador_title"><?php _e ('Casos','kmol'); ?></h1>
 			<div class="marcador_short">
+				<div class="marcador_subtitle_container">
 				<h2 class="marcador_subtitle"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-				<div class="news_meta"><?php echo "Por ".get_the_author_meta('nicename');?>, <?php kmol_posted_on();?></div>
+				<div class="news_meta"><?php echo "Por ".get_the_author_meta('nicename');?>, <?php kmol_posted_on();?></div></div>
 				 <?php if(has_post_thumbnail()){?> <div class="marcador_img"><?php the_post_thumbnail('thumbnail');?> </div> <?php }?>
 				<div class="marcador_description"><?php wpe_excerpt('wpe_excerptlength_small', 'new_excerpt_more');?></div>
 			</div>
@@ -62,8 +64,9 @@
 		<div class="grid_4 marcador omega">
 			<h1 class="marcador_title"><?php _e ('Livros','kmol'); ?></h1>
 			<div class="marcador_short">
+				<div class="marcador_subtitle_container">
 				<h2 class="marcador_subtitle"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-				<div class="news_meta"><?php echo "Por ".get_the_author_meta('nicename');?>, <?php kmol_posted_on();?></div>
+				<div class="news_meta"><?php echo "Por ".get_the_author_meta('nicename');?>, <?php kmol_posted_on();?></div></div>
 			 	<?php if(has_post_thumbnail()){?> <div class="marcador_img"><?php the_post_thumbnail('thumbnail');?> </div> <?php }?>
 				<div class="marcador_description"><?php wpe_excerpt('wpe_excerptlength_small', 'new_excerpt_more');?></div>
 			</div>
