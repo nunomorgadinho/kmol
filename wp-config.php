0e7b9f612f2c9ae0cbf475fd90a7977cc1666209
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2012-12-17 05:13:57 -0700

    config

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** Definições de MySQL - obtenha estes dados do seu serviço de alojamento** //
 /** O nome da base de dados do WordPress */
-define('DB_NAME', 'kmol');
+define('DB_NAME', 'tiratema_kmol');
 
 /** O nome do utilizador de MySQL */
-define('DB_USER', 'root');
+define('DB_USER', 'tiratema_kmol');
 
 /** A password do utilizador de MySQL  */
-define('DB_PASSWORD', 'xpto123');
+define('DB_PASSWORD', ';9X[qO)6d?k#' );
 
 /** O nome do serviddor de  MySQL  */
 define('DB_HOST', '127.0.0.1');
