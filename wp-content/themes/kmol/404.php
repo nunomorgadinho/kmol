2f37318318a8b57ce06cd72acd490b526713268c
aaires <ana.aires@gmail.com>
2012-12-17 12:37:29 +0000

    small fix

--- wp-content/themes/kmol/404.php
+++ wp-content/themes/kmol/404.php
@@ -17,30 +17,13 @@ get_header(); ?>
 
 			<article id="post-0" class="post error404 not-found">
 				<header class="entry-header">
-					<h1 class="entry-title news_title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'kmol' ); ?></h1>
+					<h1 class="entry-title news_title"><?php _e( 'Página não encontrada.', 'kmol' ); ?></h1>
 				</header><!-- .entry-header -->
 
 				<div class="entry-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try the search field?', 'kmol' ); ?></p>
+					<p><?php _e( 'Esta página não existe. Tente pesquisar pelo seu assunto de interesse.', 'kmol' ); ?></p>
 
 					<?php get_search_form(); ?>
-
-					<?php //the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-			 		<div class="widget">
-						<h2 class="widgettitle"><?php // _e( 'Most Used Categories', 'kmol' ); ?></h2>
-						<ul>
-						<?php // wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
-						</ul>
-					</div><!-- .widget --> 
-
-					<?php
-					/* translators: %1$s: smilie */
-					//$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'kmol' ), convert_smilies( ':)' ) ) . '</p>';
-					//the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php // the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
 				</div><!-- .entry-content -->
 			</article><!-- #post-0 .post .error404 .not-found -->
