34458ea36aabb56564785f49c5ed3f8722354468
aaires <ana.aires@gmail.com>
2013-01-28 15:30:00 +0000

    comments

--- wp-content/themes/kmol/page-with-comments.php
+++ wp-content/themes/kmol/page-with-comments.php
@@ -21,7 +21,7 @@ get_header(); ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<?php get_template_part( 'content', 'page' ); ?>
-OLAa
+
 					<?php  comments_template( '', true ); ?>
 
 				<?php endwhile; // end of the loop. ?>
