e78937768cdc34fe5523cc9707c8532ee9def6db
aaires <ana.aires@gmail.com>
2013-02-06 14:39:33 +0000

    popular

--- wp-content/plugins/wordpress-popular-posts/wordpress-popular-posts.php
+++ wp-content/plugins/wordpress-popular-posts/wordpress-popular-posts.php
@@ -959,7 +959,7 @@ if ( !class_exists('WordpressPopularPosts') ) {
 			
 			
 			// WPML Support
-			if (!true) {
+			if (true) {
 				$join .= ' RIGHT JOIN wp_icl_translations ON wp_posts.ID = wp_icl_translations.element_id';
 				$range .= ' AND wp_icl_translations.language_code = \'' . ICL_LANGUAGE_CODE . '\' AND wp_icl_translations.element_type = \'post_post\'';
 			}
